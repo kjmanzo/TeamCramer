b913c58c92ed46f606723dd1c986b9cf4cd20621
CramerAJ <andrew.jeffrey.cramer@gmail.com>
2017-02-16 19:59:24 -0500

    Add files via upload

---
+++ Overlay.js
@@ -0,0 +1,25 @@
+/**
+ * Created by ACramer on 2/16/17.
+ */
+$('.settings a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+
+});
+$('.home a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+
+});
+$('.friends a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+
+});
+$('.profile a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+
+});
+$('.charity a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+
+});
+
+
