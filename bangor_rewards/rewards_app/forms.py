6dc96d3e3d1a4bc041c0ef25f2c9abe06ce16136
natem926 <ntmathis@maine.rr.com>
2017-04-02 18:09:30 -0400

    added text area and typo fixes

--- bangor_rewards/rewards_app/forms.py
+++ bangor_rewards/rewards_app/forms.py
@@ -1,7 +1,10 @@
 from django import forms
+from .models import Charity
+from .models import User
 
 # https://docs.djangoproject.com/en/1.10/topics/forms/
 # read this ^
 
 class FriendForm(forms.Form):
 	friend_name = forms.CharField(label='add friend',max_length=100)
+	friend_list = forms.CharField(label='friends list',widget=forms.Textarea(attrs={'id':'reasonTextBox'}))
